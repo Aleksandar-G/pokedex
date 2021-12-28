80e572ee71ff2bb229eeb0004c9b354ee7f13782
Aleksnader.G <425730@student.fontys.nl>
2021-12-28 20:11:26 +0100

    feat: implement pagination

--- src/PokemonOverview.jsx
+++ src/PokemonOverview.jsx
@@ -2,25 +2,33 @@ import React,{useEffect,useState} from 'react'
 import axios from 'axios'
 import "./styles/PokemonOverview.css"
 import { PokemonCard } from './PokemonCard'
+import { Pagination } from "@mui/material";
 
 export const PokemonOverview = () => {
 
-    const [currPage, setCurrPage] = useState("https://pokeapi.co/api/v2/pokemon")
-    const [nextPage, setNextPage] = useState(null)
-    const [prevPage, setPrevPage] = useState(null)
+    const [currPage, setCurrPage] = useState("https://pokeapi.co/api/v2/pokemon?offset=0&limit=20")
+    const [paginationLimit, setPaginationLimit] = useState(100)
     const [pokemons, setPokemons] = useState(null)
 
 
     useEffect(() => {
         axios.get(currPage).then((res) => {
-            console.log(res.data)
+            console.log(currPage)
+            setPaginationLimit(Math.ceil(res.data.count/10))
             setPokemons(res.data.results)
-            setNextPage(res.data.next)
-            setPrevPage(res.data.prev)
+            console.log(paginationLimit)
         })
     },[currPage])
 
+    const changePage = (e,v) => {
+        setCurrPage(`https://pokeapi.co/api/v2/pokemon?offset=${20*v-20}&limit=20`)
+    }
+
     return (
-        <div className="pokemon-container ">{pokemons && pokemons.map(x => <PokemonCard key={x.name} pokemon={x} />)}</div>
+        <>
+        <div className="pokemon-container ">{pokemons && pokemons.map(x => <PokemonCard key={x.name} pokemon={x} />)}
+        </div>
+        <Pagination className="d-flex justify-content-center" count={paginationLimit} color="primary" onChange={changePage}/>
+        </>
     )
 }
