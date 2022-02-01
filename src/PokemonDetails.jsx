8653b522ed254b34b4527b83245e72cf000822f2
Aleksandar-G <72451741+Aleksandar-G@users.noreply.github.com>
2022-02-01 19:35:07 +0100

    feat(PokemonDetails.jsx/css): Add pokemon details page

--- src/PokemonDetails.jsx
+++ src/PokemonDetails.jsx
@@ -1,12 +1,56 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import "./styles/PokemonDetails.css";
 
-export const PokemonDetails = () => {
+export const PokemonDetails = (props) => {
+  const params = useParams();
 
-    const params = useParams()
-    return (
-        <div>
-            <h2>Pokemon name: {params.pokemonName}</h2>;
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    axios({
+      url: "https://pokeapi.co/api/v2/pokemon/" + params.pokemonName,
+      method: "GET",
+    }).then((res) => {
+      setData(res.data);
+      console.log(res.data);
+    });
+  }, []);
+  let content;
+
+  if (data == null) {
+    content = <h3>Loading ...</h3>;
+  } else {
+    content = (
+      <div className="pokemonDetailsContainer">
+        <div className="pokemonDetailsPictureName">
+          <h1>Pokemon name: {params.pokemonName}</h1>
+          <img
+            className="pokemonDetailsPicture"
+            src={data.sprites.front_default}
+          ></img>
         </div>
-    )
-}
+        <div className="pokemonStats">
+          <div>
+            <h2>Abilities</h2>
+            <div className="pokemonAbilities">
+              {data.abilities.map((x) => (
+                <h3 className="">{x.ability.name}</h3>
+              ))}
+            </div>
+          </div>
+          <div>
+            <h2>Types</h2>
+            <div className="typeField">
+              {data.types.map((x) => (
+                <h3>{x.type.name}</h3>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  return <div>{content}</div>;
+};
