c5e65b54c45d216ef7f2555632d200bc17e858de
Aleksandar-G <72451741+Aleksandar-G@users.noreply.github.com>
2022-02-01 19:48:12 +0100

    style: run prettier

--- src/PokemonCard.jsx
+++ src/PokemonCard.jsx
@@ -1,39 +1,48 @@
-import React, {useEffect,useState} from 'react'
-import axios from 'axios'
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import { CardActionArea } from '@mui/material';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { CardActionArea } from "@mui/material";
 
 export const PokemonCard = (props) => {
+  const [pokemonPic, setPokemonPic] = useState(null);
 
-    const [pokemonPic, setPokemonPic] = useState(null)
+  useEffect(() => {
+    axios
+      .get(process.env.REACT_APP_POKEMON_API + props.pokemon.name)
+      .then((res) => {
+        setPokemonPic(res.data.sprites.front_default);
+      });
+  }, []);
 
-    useEffect(() => {
-        axios.get("https://pokeapi.co/api/v2/pokemon/"+props.pokemon.name).then((res) => {
-            setPokemonPic(res.data.sprites.front_default)
-        })
-    }, [])
-
-    return (
-        <div className="m-3">
-        {pokemonPic &&     <Card onClick={() => (window.location.href= "http://localhost:3000/pokemon/"+props.pokemon.name)} sx={{ maxWidth: 200, minWidth:150 }}>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          image={pokemonPic}
-          alt={props.pokemon.name}
-          height="150"
-          width="150"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            {props.pokemon.name}
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>}
-        </div>
-    )
-}
+  return (
+    <div className="m-3">
+      {pokemonPic && (
+        <Card
+          onClick={() =>
+            (window.location.href =
+              "http://localhost:3000/pokemon/" + props.pokemon.name)
+          }
+          sx={{ maxWidth: 200, minWidth: 150 }}
+        >
+          <CardActionArea>
+            <CardMedia
+              component="img"
+              image={pokemonPic}
+              alt={props.pokemon.name}
+              height="150"
+              width="150"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                {props.pokemon.name}
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      )}
+    </div>
+  );
+};
--- src/PokemonOverview.jsx
+++ src/PokemonOverview.jsx
@@ -1,45 +1,42 @@
-import React,{useEffect,useState} from 'react'
-import axios from 'axios'
-import "./styles/PokemonOverview.css"
-import { PokemonCard } from './PokemonCard'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./styles/PokemonOverview.css";
+import { PokemonCard } from "./PokemonCard";
 import { Pagination } from "@mui/material";
-import { Navigate } from 'react-router-dom'
+import { Navigate } from "react-router-dom";
 
 export const PokemonOverview = () => {
+  const [currPage, setCurrPage] = useState(
+    "https://pokeapi.co/api/v2/pokemon?offset=0&limit=20"
+  );
+  const [paginationLimit, setPaginationLimit] = useState(100);
+  const [pokemons, setPokemons] = useState(null);
 
-    const [currPage, setCurrPage] = useState("https://pokeapi.co/api/v2/pokemon?offset=0&limit=20")
-    const [paginationLimit, setPaginationLimit] = useState(100)
-    const [pokemons, setPokemons] = useState(null)
+  useEffect(() => {
+    axios.get(currPage).then((res) => {
+      setPaginationLimit(Math.ceil(res.data.count / 10));
+      setPokemons(res.data.results);
+    });
+  }, [currPage]);
 
+  const changePage = (e, v) => {
+    setCurrPage(
+      `https://pokeapi.co/api/v2/pokemon?offset=${20 * v - 20}&limit=20`
+    );
+  };
 
-    useEffect(() => {
-        axios.get(currPage).then((res) => {
-            console.log(currPage)
-            setPaginationLimit(Math.ceil(res.data.count/10))
-            setPokemons(res.data.results)
-            console.log(paginationLimit)
-        })
-    },[currPage])
-
-    const viewPokemon = (name) => {
-        /*axios.get("https://pokeapi.co/api/v2/pokemon"+name).then((res) => {
-
-        })*/
-
-        <Navigate to={name} />
-
-
-    }
-
-    const changePage = (e,v) => {
-        setCurrPage(`https://pokeapi.co/api/v2/pokemon?offset=${20*v-20}&limit=20`)
-    }
-
-    return (
-        <>
-        <div className="pokemon-container ">{pokemons && pokemons.map(x => <PokemonCard key={x.name} pokemon={x} />)}
-        </div>
-        <Pagination className="d-flex justify-content-center" count={paginationLimit} color="primary" onChange={changePage}/>
-        </>
-    )
-}
+  return (
+    <>
+      <div className="pokemon-container ">
+        {pokemons &&
+          pokemons.map((x) => <PokemonCard key={x.name} pokemon={x} />)}
+      </div>
+      <Pagination
+        className="d-flex justify-content-center"
+        count={paginationLimit}
+        color="primary"
+        onChange={changePage}
+      />
+    </>
+  );
+};
